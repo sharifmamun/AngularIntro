3a0b1ecaa3eeb60307609eeceb6aab46d7feb950
sharifmamun <sharif.mamun@gmail.com>
2016-04-21 11:54:10 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,62 @@
+// Code goes here
+
+(function() {
+  var myApp = angular.module('myApp', []);
+
+  var MainController = function(
+    $scope, github, $interval, $log,
+    $anchorScroll, $location) {
+      
+    var onUserComplete = function(data) {
+      $scope.user = data;
+
+      github.getRepos($scope.user)
+        .then(onRepos, onError);
+    };
+
+    var onRepos = function(data) {
+      $scope.repos = data;
+      $location.hash("userDetails");
+      $anchorScroll();
+    };
+
+    var onError = function(reason) {
+      $scope.error = "Could not fetch data";
+    };
+
+    var decrementCountdown = function() {
+      $scope.countdown -= 1;
+      if ($scope.countdown < 1) {
+        $scope.search($scope.username);
+      }
+    };
+
+    var countdownInterval = null;
+    var startCountdown = function() {
+      countdownInterval = $interval(decrementCountdown, 1000, $scope.countdown);
+
+    };
+
+    $scope.search = function(username) {
+      $log.info("Searching for .. " + username);
+
+      github.getUser(username)
+        .then(onUserComplete, onError);
+
+      if (countdownInterval) {
+        $interval.cancel(countdownInterval);
+        $scope.countdown = null;
+      }
+    };
+
+    $scope.username = "angular";
+    $scope.message = "GitHub Viewer!";
+    $scope.repoSortOrder = "-stargazers_count";
+    $scope.countdown = null;
+
+    startCountdown();
+  };
+
+  myApp.controller('MainController', ["$scope", "github", "$interval", "$log", "$anchorScroll", "$location", MainController]);
+
+}());
