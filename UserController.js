454aed8019798fbf7c212da1b07ea7e9e76e821f
sharifmamun <sharif.mamun@gmail.com>
2016-04-21 17:17:04 -0700

    added routes and styles

---
+++ UserController.js
@@ -0,0 +1,30 @@
+// Code goes here
+
+(function() {
+  var myApp = angular.module('githubViewer');
+
+  var UserController = function($scope, github, $routeParams) {
+      
+    var onUserComplete = function(data) {
+      $scope.user = data;
+
+      github.getRepos($scope.user)
+        .then(onRepos, onError);
+    };
+
+    var onRepos = function(data) {
+      $scope.repos = data;
+    };
+
+    var onError = function(reason) {
+      $scope.error = "Could not fetch data";
+    };
+
+    $scope.username = $routeParams.username;
+    $scope.repoSortOrder = "-stargazers_count";
+    github.getUser($scope.username).then(onUserComplete, onError);
+  };
+
+  myApp.controller('UserController', UserController);
+
+}());
